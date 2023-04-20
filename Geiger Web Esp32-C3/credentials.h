76b4a425f6bb4362f2757ff7092679a02e981d76
Jordi Rossell <4Xsample@gmail.com>
2023-04-20 21:23:37 +0200

    credentials.h

---
+++ Geiger Web Esp32-C3/credentials.h
@@ -0,0 +1,15 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Credencials de la wifi
+#define WIFI_SSID "Nom de la teva wifi"
+#define WIFI_PASSWORD "Codi de la teva wifi"
+
+// Credencials de la base de dades
+#define DB_IP "192.168.1.100"  // Adreça IP de la base de dades
+#define DB_PORT 3306  // Port de la base de dades
+#define DB_NAME "nom_de_la_base_de_dades"  // Nom de la base de dades
+#define DB_USER "usuari_de_la_base_de_dades"  // Nom d'usuari de la base de dades
+#define DB_PASSWORD "contrasenya_de_la_base_de_dades"  // Contrasenya de l'usuari de la base de dades
+
+#endif
